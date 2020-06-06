8b5f175f7bda295a9c8b3fe7bfdc6e4253b26aa1
Matt Black <github@mafro.net>
2020-06-06 16:27:36 +1000

    [feature] Pull full issue history

--- jira_offline/models.py
+++ jira_offline/models.py
@@ -168,6 +168,15 @@ class AppConfig(DataclassSerializer):
             f.write('\n')
 
 
+@dataclass
+class HistoryItem(DataclassSerializer):
+    author: str = field(metadata={'readonly': True})
+    history_ts: datetime.datetime = field(metadata={'readonly': True})
+    field_name: str = field(metadata={'readonly': True, 'raw': 'field'})
+    from_value: Optional[str] = field(default=None, metadata={'readonly': True, 'raw': 'fromString'})
+    to_value: Optional[str] = field(default=None, metadata={'readonly': True, 'raw': 'toString'})
+
+
 @dataclass  # pylint: disable=too-many-instance-attributes
 class Issue(DataclassSerializer):  # pylint: disable=too-many-instance-attributes
     project_id: str = field(metadata={'friendly': 'Project ID', 'readonly': True})
@@ -194,6 +203,7 @@ class Issue(DataclassSerializer):  # pylint: disable=too-many-instance-attribute
         default=None, metadata={'friendly': 'Status', 'property': 'status', 'readonly': True}
     )
     updated: Optional[datetime.datetime] = field(default=None, metadata={'readonly': True})
+    history: Optional[List[HistoryItem]] = field(default=None, metadata={'readonly': True})
 
     # local-only dict which represents serialized Issue last seen on Jira server
     # this property is not written to cache and is created at runtme from diff_to_original
@@ -373,6 +383,7 @@ class Issue(DataclassSerializer):  # pylint: disable=too-many-instance-attribute
             *fmt('creator'),
             *fmt('created'),
             *fmt('updated'),
+            *fmt('history'),
         ]
 
 
